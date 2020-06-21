35cbae7b68ef9d07af816f493374cd94c8424f75
Petro Fedorenko <petewaitforletters@gmail.com>
2020-06-21 13:52:06 +0300

    add getStarships

--- src/services/swApiService.js
+++ src/services/swApiService.js
@@ -1,9 +1,35 @@
-const url = 'https://swapi.dev/api'
+const url = "https://swapi.dev/api";
 
 export const getPeople = async () => {
-    const peopleResponse = await (await fetch(`${url}/people`)).json();
+  const peopleResponse = await (await fetch(`${url}/people`)).json();
 
-    return  peopleResponse.results.map(({name, height, mass, gender, birth_year}) => ({
-        name, height, mass, gender, birth_year
-    }))
-}
+  return peopleResponse.results.map(
+    ({ name, height, mass, gender, birth_year }) => ({
+      name,
+      height,
+      mass,
+      gender,
+      birth_year,
+    })
+  );
+};
+
+export const getStarships = async () => {
+  const starshipsResponse = await (await fetch(`${url}/starships`)).json();
+
+  return starshipsResponse.results.map(
+    ({
+      name,
+      model,
+      manufacturer,
+      cost_in_credits,
+      max_atmosphering_speed,
+    }) => ({
+      name,
+      model,
+      manufacturer,
+      cost_in_credits,
+      max_atmosphering_speed,
+    })
+  );
+};
