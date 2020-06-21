c95940288b6f281cd78afc336121d654d348f474
Petro Fedorenko <petewaitforletters@gmail.com>
2020-06-21 15:47:58 +0300

    implement PlanetsPage

--- src/components/pages/FormPage.jsx
+++ src/components/pages/FormPage.jsx
@@ -5,21 +5,39 @@ import Form from "../common/Form";
 const FormPage = () => {
   const [context] = useContext(Context);
 
+  const getColumns = () => {
+    switch (context.item.type) {
+      case "people":
+        return context.getColumnNames();
+      case "starships":
+        return context.getStarshipColumnNames();
+      case "planets":
+        return context.getPlanetColumnNames();
+      default:
+        return [];
+    }
+  };
+
+  const getOnAddData = () => {
+    switch (context.item.type) {
+      case "people":
+        return context.handleAppPerson;
+      case "starships":
+        return context.handleAddStarship;
+      case "planets":
+        return context.handleAddPlanet;
+      default:
+        return [];
+    }
+  };
+
   return (
     <>
       <h2>Form Page</h2>
       <Form
         initialData={context.item}
-        columns={
-          context.item.type === "people"
-            ? context.getColumnNames()
-            : context.getStarshipColumnNames()
-        }
-        onAddData={
-          context.item.type === "people"
-            ? context.handleAppPerson
-            : context.handleAddStarship
-        }
+        columns={getColumns()}
+        onAddData={getOnAddData()}
       />
     </>
   );
